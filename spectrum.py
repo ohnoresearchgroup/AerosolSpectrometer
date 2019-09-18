a318339326a0dd24846fd14da86308166790e28b
Paul Ohno <paul.e.ohno@gmail.com>
2019-09-18 16:28:06 -0400

    edited spectrum to print average, acquired spectrum

--- spectrum.py
+++ spectrum.py
@@ -36,12 +36,12 @@ class Spectrum():
         
     def createDetailsFile(self):  
         f_details  = open(self.dirpath + '\\' + self.time +'_details.txt','w')
-        f_details.write('name\t' + self.name + '\r\n')
-        f_details.write('time\t' + self.time + '\r\n')
-        f_details.write('start\t' + str(self.start) + ' nm\r\n')
-        f_details.write('stop\t' + str(self.stop) + ' nm\r\n')
-        f_details.write('step\t' + str(self.step) + ' nm\r\n')
-        f_details.write('duration\t' + str(self.duration) + ' s\r\n')
+        f_details.write('name\t' + self.name + '\n')
+        f_details.write('time\t' + self.time + '\n')
+        f_details.write('start\t' + str(self.start) + ' nm\n')
+        f_details.write('stop\t' + str(self.stop) + ' nm\n')
+        f_details.write('step\t' + str(self.step) + ' nm\n')
+        f_details.write('duration\t' + str(self.duration) + ' s\n')
         f_details.close()
     
     def runScan(self):
@@ -68,12 +68,13 @@ class Spectrum():
             #add the array of data to counts, write to file          
             self.counts.append(data)            
             string = np.array2string(data,separator='\t')[1:][:-1]
-            f_data.write('\t' + string + '\r\n')
+            f_data.write('\t' + string + '\n')
             
             #add the mean to aves, write to file
             ave = np.mean(data)
             self.aves.append(ave)
-            f_ave.write('\t' + str(ave) + '\r\n')
+            f_ave.write('\t' + str(ave) + '\n')
+            print('Ave = ' + str(ave) + ' c.p.s.')
             
         f_ave.close()
         f_data.close()
